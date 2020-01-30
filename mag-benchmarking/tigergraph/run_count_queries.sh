fa2af7196e6220ce40de2e95d1af70ebb1ac1497
Dimitar Nikolov <dimitar.g.nikolov@gmail.com>
2020-01-30 17:42:41 +0000

    streamline run scripts, clean up drop scripts

--- mag-benchmarking/tigergraph/run_count_queries.sh
+++ mag-benchmarking/tigergraph/run_count_queries.sh
@@ -1,17 +1,20 @@
-echo "Running count_all_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_all_nodes()"
-echo "Running count_paper_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_paper_nodes()"
-echo "Running count_author_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_author_nodes()"
-echo "Running count_affiliation_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_affiliation_nodes()"
-echo "Running count_journal_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_journal_nodes()"
-echo "Running count_fos_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_fos_nodes()"
-echo "Running count_confinstance_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_confinstance_nodes()"
-echo "Running count_confseries_nodes" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_confseries_nodes()"
-echo "Running count_all_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_all_edges()"
-echo "Running count_is_affiliated_with_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_is_affiliated_with_edges()"
-echo "Running count_is_author_of_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_is_author_of_edges()"
-echo "Running count_is_instance_of_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_is_instance_of_edges()"
-echo "Running count_belongs_to_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_belongs_to_edges()"
-echo "Running count_is_presented_at_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_is_presented_at_edges()"
-echo "Running count_is_published_in_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_is_published_in_edges()"
-echo "Running count_refers_to_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_refers_to_edges()"
-echo "Running count_refers_is_referred_by_edges" && time gsql -g mag "USE GRAPH mag SET query_timeout=240000 RUN QUERY count_is_referred_by_edges()"
+S=http://localhost:9000/query/mag
+H="GSQL-TIMEOUT: 120000"
+
+echo "Running count_all_nodes" && time curl -H "$H" $S/count_all_nodes
+echo "Running count_paper_nodes" && time curl -H "$H" $S/count_paper_nodes
+echo "Running count_author_nodes" && time curl -H "$H" $S/count_author_nodes
+echo "Running count_affiliation_nodes" && time curl -H "$H" $S/count_affiliation_nodes
+echo "Running count_journal_nodes" && time curl -H "$H" $S/count_journal_nodes
+echo "Running count_fos_nodes" && time curl -H "$H" $S/count_fos_nodes
+echo "Running count_confinstance_nodes" && time curl -H "$H" $S/count_confinstance_nodes
+echo "Running count_confseries_nodes" && time curl -H "$H" $S/count_confseries_nodes
+echo "Running count_all_edges" && time curl -H "$H" $S/count_all_edges
+echo "Running count_is_affiliated_with_edges" && time curl -H "$H" $S/count_is_affiliated_with_edges
+echo "Running count_is_author_of_edges" && time curl -H "$H" $S/count_is_author_of_edges
+echo "Running count_is_instance_of_edges" && time curl -H "$H" $S/count_is_instance_of_edges
+echo "Running count_belongs_to_edges" && time curl -H "$H" $S/count_belongs_to_edges
+echo "Running count_is_presented_at_edges" && time curl -H "$H" $S/count_is_presented_at_edges
+echo "Running count_is_published_in_edges" && time curl -H "$H" $S/count_is_published_in_edges
+echo "Running count_refers_to_edges" && time curl -H "$H" $S/count_refers_to_edges
+echo "Running count_is_referred_by_edges" && time curl -H "$H" $S/count_is_referred_by_edges
