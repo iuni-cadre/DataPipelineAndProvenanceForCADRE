a50e8caadcf3a4872c0c14a56fdb4add8050e24f
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2020-03-10 14:56:11 -0400

    Update preprocessing.bash

--- wosParseYan/preprocessing.bash
+++ wosParseYan/preprocessing.bash
@@ -3,11 +3,11 @@ ssh iuni2.carbonate.uits.iu.edu
 export HADOOP_USER_NAME=hdfs
 export PATH=$PATH:~/.local/bin
 
-cd /N/project/iuni_cadre/wos_raw/wos_17_18
-unzip ESCI_2005_2019_Annuals/'*.zip' -d xmlRaw
-unzip CORE_1900-2018/'*.zip' -d xmlRaw
+cd /N/project/iuni_cadre/WoS_18_19
+unzip 'ESCI 2005_2019/'*.zip' -d xmlRaw
+unzip 'CORE 1900-2019'/'*.zip' -d xmlRaw
 
 hdfs dfs -mkdir /WoSraw/
-hdfs dfs -copyFromLocal xmlRaw/ /WoSraw/
+hdfs dfs -copyFromLocal xmlRaw/ /WoSraw/WoS2019
 hdfs dfs -ls /
 hdfs dfs -copyToLocal <hdfs_input_file_path> <output_path>
