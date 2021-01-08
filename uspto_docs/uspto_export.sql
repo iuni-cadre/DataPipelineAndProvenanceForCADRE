658463610e12f9f30dfa427572ef13c7ba0d9186
Matthew Hutchinson <maahutch@iu.edu>
2021-01-08 13:57:38 -0500

    updatting sql queries to handle unusual characters in text

--- uspto_docs/uspto_export.sql
+++ uspto_docs/uspto_export.sql
@@ -26,9 +26,9 @@ SELECT inventor_id,
 	   years_active
 FROM inventor
 INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/inventor.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- Location
@@ -57,12 +57,12 @@ SELECT  location_id,
 		num_patents
 FROM location
 INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/location.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
--- Assignee******
+-- Assignee
 SELECT  'assignee_id',
 		'type', 
 		'name_first', 
@@ -88,9 +88,9 @@ SELECT  assignee_id,
 		persistent_assignee_id
 FROM assignee
 INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/assignee.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- Government Organization
@@ -103,9 +103,9 @@ UNION
 SELECT * 
 FROM government_organization
 INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/government_organization.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- Examiner
@@ -119,9 +119,9 @@ UNION
 SELECT *
 FROM examiner
 INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/examiner.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- Application
@@ -138,9 +138,9 @@ SELECT  application_id,
 		date
 FROM application
 INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/application.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- Lawyer
@@ -169,13 +169,14 @@ SELECT  lawyer_id,
 		persistent_lawyer_id
 FROM lawyer
 INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/lawyer.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 
 -- Patent
+SET NAMES utf8;
 SELECT  'patent_id',
 		'type', 
 		'number', 
@@ -198,32 +199,30 @@ SELECT  'patent_id',
 		'term_extension', 
 		'detail_desc_length'
 UNION
-SELECT  patent_id,
-		type, 
-		number, 
-		country, 
-		date, 
-		year, 
-		abstract, 
-		title, 
-		kind, 
-		num_claims, 
-		num_foreign_documents_cited, 
-		num_us_applications_cited, 
-		num_us_patents_cited, 
-		num_total_documents_cited, 
-		num_times_cited_by_us_patents, 
-		earliest_application_date, 
-		patent_processing_days, 
-		uspc_current_mainclass_average_patent_processing_days, 
-		cpc_current_group_average_patent_processing_days,
-		term_extension, 
-		detail_desc_length
+SELECT  ifnull(patent_id,'NULL'),
+		ifnull(type, 'NULL'),
+		ifnull(number, 'NULL'),
+		ifnull(country, 'NULL'),
+		ifnull(date, 'NULL'),
+		ifnull(year, 'NULL'),
+		ifnull(abstract, 'NULL'),
+		ifnull(title, 'NULL'),
+		ifnull(kind, 'NULL'),
+		ifnull(num_claims,'NULL'), 
+		ifnull(num_foreign_documents_cited, 'NULL'),
+		ifnull(num_us_applications_cited, 'NULL'),
+		ifnull(num_us_patents_cited, 'NULL'),
+		ifnull(num_total_documents_cited, 'NULL'),
+		ifnull(num_times_cited_by_us_patents, 'NULL'),
+		ifnull(earliest_application_date, 'NULL'),
+		ifnull(patent_processing_days, 'NULL'),
+		ifnull(uspc_current_mainclass_average_patent_processing_days, 'NULL'),
+		ifnull(cpc_current_group_average_patent_processing_days, 'NULL'),
+		ifnull(term_extension, 'NULL'),
+		ifnull(detail_desc_length,'NULL')
 FROM patent
-INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/patent.tsv'
-FIELDS ENCLOSED BY '"' 
-TERMINATED BY '\t' 
-ESCAPED BY '"' 
+INTO OUTFILE '/tmp/csv_for_janusgraph/nodes/patent2.tsv'
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' ESCAPED BY '\\' 
 LINES TERMINATED BY '\r\n';
 
 
@@ -239,9 +238,9 @@ SELECT  location_id,
 		inventor_id
 FROM  location_inventor
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/inventor_located_in.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- ASSIGNEE_LOCATED_IN
@@ -252,9 +251,9 @@ SELECT  location_id,
 		assignee_id
 FROM location_assignee
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/assignee_located_in.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 
@@ -266,9 +265,9 @@ SELECT  inventor_id,
 		coinventor_id
 FROM inventor_coinventor
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/coinventor_with.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 
@@ -280,9 +279,9 @@ SELECT  patent_id,
 		inventor_id
 FROM patent_inventor
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/inventor_of.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- ASSIGNED_TO
@@ -293,9 +292,9 @@ SELECT  patent_id,
 		assignee_id
 FROM patent_assignee
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/assigned_to.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- LAWYER_OF
@@ -306,9 +305,9 @@ SELECT  patent_id,
  		lawyer_id
 FROM patent_lawyer
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/lawyer_of.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- INTERESTED_IN
@@ -319,9 +318,9 @@ SELECT  patent_id,
 		organization_id
 FROM patent_govintorg
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/interest_in.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- EXAMINER_OF
@@ -332,9 +331,9 @@ SELECT  patent_id,
 		examiner_id
 FROM patent_examiner
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/examiner_of.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 
@@ -346,9 +345,9 @@ SELECT  citing_patent_id,
 		cited_application_id
 FROM usapplicationcitation
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/app_cites_patents.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- APPLICATION -> BECOMES -> PATENT
@@ -359,9 +358,9 @@ SELECT  application_id,
 		patent_id
 FROM application
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/becomes.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 
@@ -373,9 +372,9 @@ SELECT 	citing_patent_id,
 		cited_patent_id
 FROM uspatentcitation
 INTO OUTFILE '/tmp/csv_for_janusgraph/edges/patent_cites_patent.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 
@@ -395,9 +394,9 @@ SELECT DISTINCT(group_id) AS 'cpc_id',  'group' AS 'level', group_title AS 'labe
 UNION ALL
 SELECT subgroup_id AS 'cpc_id', 'subgroup' AS 'level', subgroup_title AS 'label' FROM cpc_current
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/cpc_node.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 -- CPC - Edge
@@ -411,34 +410,30 @@ SELECT patent_id, group_id AS 'cpc_id' FROM cpc_current
 UNION ALL
 SELECT patent_id, subgroup_id AS 'cpc_id' FROM cpc_current
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/cpc_to_patent.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';
 
 
 -- USPC - Node
 SELECT 'uspc_id', 'level', 'label'
 UNION
-SELECT DISTINCT(mainclass_id) AS 'uspc_id', 'mainclass' AS 'level', mainclass_title AS 'label' FROM uspc_current
+SELECT DISTINCT(ifnull(mainclass_id,'')) AS 'uspc_id', 'mainclass' AS 'level', ifnull(mainclass_title,'') AS 'label' FROM uspc_current
 UNION ALL
-SELECT DISTINCT(subclass_id) AS 'uspc_id', 'subclass' AS 'level', subclass_id AS 'label' FROM uspc_current
-INTO OUTFILE '/tmp/csv_for_janusgraph/categories/uspc_node.tsv'
-FIELDS ENCLOSED BY '"' 
-TERMINATED BY '\t' 
-ESCAPED BY '"' 
+SELECT DISTINCT(ifnull(subclass_id,''))  AS 'uspc_id', 'subclass' AS 'level', ifnull(subclass_id,'') AS 'label' FROM uspc_current
+INTO OUTFILE '/tmp/csv_for_janusgraph/categories/uspc_node2.tsv'
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' ESCAPED BY '\\' 
 LINES TERMINATED BY '\r\n';
 
 -- USPC - Edge 
 SELECT 'patent_id', 'uspc_id'
 UNION
-SELECT patent_id, mainclass_id AS 'uspc_id' FROM uspc_current
+SELECT ifnull(patent_id, ''), ifnull(mainclass_id, '') AS 'uspc_id' FROM uspc_current
 UNION ALL 
-SELECT patent_id, subclass_id AS 'uspc_id' FROM uspc_current
-INTO OUTFILE '/tmp/csv_for_janusgraph/categories/uspc_to_patent.tsv'
-FIELDS ENCLOSED BY '"' 
-TERMINATED BY '\t' 
-ESCAPED BY '"' 
+SELECT ifnull(patent_id,''), ifnull(subclass_id,'') AS 'uspc_id' FROM uspc_current
+INTO OUTFILE '/tmp/csv_for_janusgraph/categories/uspc_to_patent2.tsv'
+FIELDS TERMINATED BY '\t' ENCLOSED BY '"' ESCAPED BY '\\' 
 LINES TERMINATED BY '\r\n';
 
 -- WIPO - Node
@@ -448,9 +443,9 @@ SELECT DISTINCT(wipo_field_id), 'field_title' AS 'level', wipo_field_title AS 'l
 UNION ALL
 SELECT DISTINCT(wipo_field_id), 'sector_title' AS 'level', wipo_sector_title AS 'label' FROM wipo_entity
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/wipo_node.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';	
 
 -- WIPO - Edge
@@ -458,9 +453,9 @@ SELECT 'patent_id', 'wipo_field_id'
 UNION
 SELECT patent_id, wipo_field_id FROM wipo_entity
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/wipo_to_patent.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';	
 
 
@@ -471,9 +466,9 @@ SELECT DISTINCT(category_id) AS 'nber_id', 'category' AS 'level', category_title
 UNION ALL 
 SELECT DISTINCT(subcategory_id) AS 'nber_id', 'sub_category' AS 'level', subcategory_title AS 'label' FROM nber
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/nber_node.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';	
 
 -- NBER - Edge
@@ -483,9 +478,9 @@ SELECT patent_id, category_id AS 'nber_id' FROM nber
 UNION ALL
 SELECT patent_id, subcategory_id AS 'nber_id' FROM nber
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/nber_to_patent.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';	
 
 
@@ -508,9 +503,9 @@ SELECT DISTINCT(sequence) AS 'ipcr_id', 'classification_value'       AS 'level',
 UNION ALL
 SELECT DISTINCT(sequence) AS 'ipcr_id', 'classification_data_source' AS 'level', classification_data_source AS 'label' FROM ipcr
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/ipcr_node.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';	
 
 
@@ -519,9 +514,9 @@ SELECT 'patent_id', 'sequence'
 UNION
 SELECT patent_id, sequence FROM ipcr
 INTO OUTFILE '/tmp/csv_for_janusgraph/categories/ipcr_to_patent.tsv'
-FIELDS ENCLOSED BY '"' 
+FIELDS ENCLOSED BY '' 
 TERMINATED BY '\t' 
-ESCAPED BY '"' 
+ESCAPED BY '' 
 LINES TERMINATED BY '\r\n';	
 
 
