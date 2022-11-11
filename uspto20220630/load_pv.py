af5a2c665c9fb716fc927d8132d8b477bd658bc7
Jacob Shaw <shaw10@iuni1.colo.carbonate.local>
2022-11-11 17:29:08 -0500

    adding information output for load script

--- uspto20220630/load_pv.py
+++ uspto20220630/load_pv.py
@@ -7,13 +7,18 @@ conn = create_engine('postgresql://cadre_admin@localhost:5432/core_data').connec
 path = '/raw/PatentsView20220630/'
 files = listdir(path)
 
-for f in files:
+for f in files[2:4]:
     tab = f[:-4]
 
+    print('loading data', tab)
     data = pd.read_csv(path+f,sep='\t',low_memory=False, on_bad_lines='error')
 
     if tab == 'g_application':
-        data['file_47_flag'] = data['file_47_flag'].map(lambda x: True if x == 1.0 else False)
+        data['rule_47_flag'] = data['rule_47_flag'].map(lambda x: True if x == 1.0 else False)
     
-    #data.to_sql(tab, conn, schema='patview_core',index=False, if_exists='append')
-    print(data)
+    try:
+        print('starting inserts for', tab)
+#        data.to_sql(tab, conn, schema='patview_core',index=False, if_exists='append')
+	print('inserts complete for', tab)
+    except ValueError as e:
+        print(e)
