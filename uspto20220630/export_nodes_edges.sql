848a56783d2f88fefeadfb10d1b24d3cb267f091
Shaw <shaw10@iu.edu>
2022-11-16 12:08:42 -0600

    correcting MySQL to PSQL translations

--- uspto20220630/export_nodes_edges.sql
+++ uspto20220630/export_nodes_edges.sql
@@ -12,7 +12,7 @@ SELECT 'inventor_id',
         'first_seen_date',
         'last_seen_date',
         'years_active'
-UNION
+UNION ALL
 SELECT inventor_id,
         disambig_inventor_name_first,
         disambig_inventor_name_last,
@@ -36,15 +36,15 @@ SELECT 'location_id',
         'num_assignees',
         'num_inventors',
         'num_patents'
-UNION
+UNION ALL
 SELECT location_id,
         disambig_city,
         disambig_state,
         disambig_country,
         state_fips,
         county_fips,
-        latitude,
-        longitude,
+        latitude::VARCHAR(256),
+        longitude::VARCHAR(256),
         'NULL', -- num_assignees no longer supported
         'NULL', -- num_invenotrs no longer supported
         'NULL' -- num_patents no longer supported
@@ -62,9 +62,9 @@ SELECT 'assignee_id',
         'last_seen_date',
         'years_active',
         'persistent_assignee_id'
-UNION
+UNION ALL
 SELECT a.assignee_id,
-        a.assignee_type,
+        a.assignee_type::VARCHAR(64),
         a.disambig_assignee_individual_name_first,
         a.disambig_assignee_individual_name_last,
         a.disambig_assignee_organization,
@@ -73,10 +73,14 @@ SELECT a.assignee_id,
         'NULL', -- first seen date no longer supported
         'NULL', -- last seen date no longer supported
         'NULL', -- years active no longer supported
-        p.rawassignee_uuid, -- get from g_persistent_assignee
+        p.rawassignee_uuid -- get from g_persistent_assignee
 FROM patview_core.g_assignee_disambiguated a
 LEFT JOIN patview_core.g_persistent_assignee p
-ON a.assignee_id = p.disamb_inventor_id_xxxxxxxx;
+ON a.assignee_id in ( p.disamb_assignee_id_20181127,p.disamb_assignee_id_20190312,p.disamb_assignee_id_20190820,
+                        p.disamb_assignee_id_20191008,p.disamb_assignee_id_20191231,p.disamb_assignee_id_20200331,
+                        p.disamb_assignee_id_20200630,p.disamb_assignee_id_20200929,p.disamb_assignee_id_20201229,
+                        p.disamb_assignee_id_20210330,p.disamb_assignee_id_20210629,p.disamb_assignee_id_20210708,
+                        p.disamb_assignee_id_20210930,p.disamb_assignee_id_20211230,p.disamb_assignee_id_20220630);
 
 -- Government Organization
 SELECT 'organization_id',
@@ -85,9 +89,9 @@ SELECT 'organization_id',
         'level_two',
         'level_three',
         'gi_statement'
-UNION
-SELECT org.gi_organization_id,
-        org.name,
+UNION ALL
+SELECT org.gi_organization_id::VARCHAR(64),
+        org.fedagency_name,
         org.level_one,
         org.level_two,
         org.level_three,
@@ -103,10 +107,10 @@ SELECT 'examiner_id',
         'role',
         'group',
         'persistent_examiner_id'
-UNION
+UNION ALL
 SELECT NULL, -- examiner id no longer supported
         raw_examiner_name_first,
-        raw_applicant_name_last,
+        raw_examiner_name_last,
         examiner_role,
         art_group,
         'NULL' -- persistent examiner id no longer supported
@@ -118,7 +122,7 @@ SELECT 'application_id',
         'number',
         'country',
         'date'
-UNION
+UNION ALL
 SELECT application_id,
         patent_application_type,
         'NULL', -- number no longer supported
@@ -138,7 +142,7 @@ SELECT 'lawyer_id',
         'last_seen_date',
         'years_active',
         'persistent_lawyer_id'
-UNION
+UNION ALL
 SELECT attorney_id,
         disambig_attorney_name_first,
         disambig_attorney_name_last,
@@ -174,17 +178,17 @@ SELECT 'patent_id',
         'cpc_current_group_average_patent_processing_days',
         'term_extension',
         'detail_desc_length'
-UNION
-SELECT ifnull(patent_id,'NULL'),
-        ifnull(patent_type,'NULL'),
+UNION ALL
+SELECT COALESCE(patent_id,'NULL'),
+        COALESCE(patent_type,'NULL'),
         'NULL', -- number no longer supported
         'NULL', -- country no longer supported
-        ifnull(patent_date,'NULL'),
-        ifnull(extract(year from date(patent_date)), 'NULL'),
-        ifnull(patent_abstract, 'NULL'),
-        ifnull(patent_title,'NULL'),
-        ifnull(wipo_kind, 'NULL'),
-        ifnull(num_claims, 'NULL'),
+        COALESCE(patent_date,'NULL'),
+        COALESCE(extract(year from date(patent_date))::VARCHAR(4), 'NULL'),
+        COALESCE(patent_abstract, 'NULL'),
+        COALESCE(patent_title,'NULL'),
+        COALESCE(wipo_kind, 'NULL'),
+        COALESCE(num_claims::VARCHAR(64), 'NULL'),
         'NULL', -- num foreign documents cited no longer supported
         'NULL', -- num us applications cited no longer supported
         'NULL', -- num us patents cited no longer supported
@@ -205,7 +209,7 @@ FROM patview_core.g_patent;
 -- INVENTOR LOCATED IN
 SELECT 'location_id',
         'inventor_id'
-UNION
+UNION ALL
 SELECT location_id,
         inventor_id
 FROM patview_core.g_inventor_disambiguated;
@@ -213,7 +217,7 @@ FROM patview_core.g_inventor_disambiguated;
 -- ASSIGNEE LOCATED IN
 SELECT 'location_id',
         'assignee_id'
-UNION
+UNION ALL
 SELECT location_id,
         assignee_id
 FROM patview_core.g_assignee_disambiguated;
@@ -221,7 +225,7 @@ FROM patview_core.g_assignee_disambiguated;
 -- COINVENTOR OF 
 SELECT 'inventor_id',
         'coinventor_id'
-UNION
+UNION ALL
 SELECT a.inventor_id as inventor_id,
         b.inventor_id as coinventor_id
 FROM patview_core.g_inventor_disambiguated a
@@ -231,7 +235,7 @@ ON a.patent_id = b.patent_id AND a.inventor_id <> b.inventor_id;
 -- INVENTOR OF
 SELECT 'patent_id',
         'inventor_id'
-UNION
+UNION ALL
 SELECT patent_id,
         inventor_id
 FROM patview_core.g_inventor_disambiguated;
@@ -239,7 +243,7 @@ FROM patview_core.g_inventor_disambiguated;
 -- Assigned to
 SELECT 'patent_id',
         'assignee_id'
-UNION
+UNION ALL
 SELECT patent_id,
         assignee_id
 FROM patview_core.g_assignee_disambiguated;
@@ -247,7 +251,7 @@ FROM patview_core.g_assignee_disambiguated;
 -- Lawyer of
 SELECT 'patent_id',
         'lawyer_id'
-UNION
+UNION ALL
 SELECT patent_id,
         attorney_id
 from patview_core.g_attorney_disambiguated;
@@ -255,17 +259,17 @@ from patview_core.g_attorney_disambiguated;
 --Interested in 
 SELECT 'patent_id',
         'gi_organization_id'
-UNION
-SELECT UPPER(org.patent_id),
-        UPPER(org.gi_organization_id)
+UNION ALL
+SELECT UPPER(org.patent_id::VARCHAR(20)),
+        UPPER(org.gi_organization_id::VARCHAR(64))
 FROM patview_core.g_gov_interest_org org
-JOIN g_patent p
+JOIN patview_core.g_patent p
 ON p.patent_id = org.patent_id;
 
 -- Examiner of
 SELECT 'patent_id',
         'examiner_id'
-UNION
+UNION ALL
 SELECT e.patent_id,
         'NULL'
 FROM patview_core.g_examiner_not_disambiguated e
@@ -275,7 +279,7 @@ ON p.patent_id = e.patent_id;
 -- Application -> Cites -> Patents
 SELECT 'citing_patent_id',
         'cited_application_id'
-UNION
+UNION ALL
 SELECT cit.patent_id,
         app.application_id
 FROM patview_core.g_us_application_citation cit
@@ -285,22 +289,22 @@ ON cit.patent_id = app.patent_id;
 -- Application -> Becomes -> Patent
 SELECT 'application_id',
         'patent_id'
-UNION
+UNION ALL
 SELECT application_id,
         patent_id
-FROM patview_core.application;
+FROM patview_core.g_application;
 
 -- Patent -> Cites -> Patentes
 SELECT 'citing_patent_id',
         'cited_patent_id'
-UNION
+UNION ALL
 SELECT UPPER(c.patent_id),
         UPPER(c.citation_patent_id)
 FROM patview_core.g_us_patent_citation c
-JOIN patent p1
-ON p1.patent = c.patent_id
-JOIN patent p2
-ON p2 = c.citation_patent_id;
+JOIN patview_core.g_patent p1
+ON p1.patent_id = c.patent_id
+JOIN patview_core.g_patent p2
+ON p2.patent_id = c.citation_patent_id;
 
 -- ############### --
 --  CATEGORY DATA  --
@@ -310,116 +314,117 @@ ON p2 = c.citation_patent_id;
 SELECT 'cpc_id',
         'level',
         'label'
-UNION
-SELECT DISTINCT(cpc_section) AS 'cpc_id',
-        'section' AS 'level',
-        cpc_section AS 'label'
+UNION ALL
+SELECT DISTINCT(cpc_section) AS cpc_id,
+        'section' AS level,
+        cpc_section AS label
 FROM patview_core.g_cpc_current
 UNION ALL
-SELECT DISTINCT(c.cpc_class) AS 'cpc_id',
-        'subsection' AS 'level',
-        t.cpc_class_title AS 'label'
+SELECT DISTINCT(c.cpc_class) AS cpc_id,
+        'subsection' AS level,
+        t.cpc_class_title AS label
 FROM patview_core.g_cpc_current c
 JOIN patview_core.g_cpc_title t
 ON c.cpc_group = t.cpc_group
 UNION ALL
-SELECT DISTINCT(c.cpc_subclass) AS 'cpc_id',
-        'group' as 'level',
-        t.cpc_subclass_title AS 'label'
+SELECT DISTINCT(c.cpc_subclass) AS cpc_id,
+        'group' as level,
+        t.cpc_subclass_title AS label
 FROM patview_core.g_cpc_current c
 JOIN patview_core.g_cpc_title t
 ON c.cpc_group = t.cpc_group
 UNION ALL
-SELECT c.cpc_group AS 'cpc_id',
-        'subgroup' AS 'level',
-        t.cpc_group_title AS 'label'
+SELECT c.cpc_group AS cpc_id,
+        'subgroup' AS level,
+        t.cpc_group_title AS label
 FROM patview_core.g_cpc_current c
-JOIN patview_core.g_cpc_title;
+JOIN patview_core.g_cpc_title t
+ON c.cpc_group = t.cpc_group;
 
 -- CPC Edge
 SELECT 'patent_id', 'cpc_id'
-UNION
-SELECT patent_id, cpc_section as 'cpc_id'
+UNION ALL
+SELECT patent_id, cpc_section AS cpc_id
 FROM patview_core.g_cpc_current
-UNION
-SELECT patent_id, cpc_class AS 'cpc_id'
+UNION ALL
+SELECT patent_id, cpc_class AS cpc_id
 FROM patview_core.g_cpc_current
-UNION
-SELECT patent_id, cpc_subclass AS 'cpc_id'
+UNION ALL
+SELECT patent_id, cpc_subclass AS cpc_id
 FROM patview_core.g_cpc_current
-UNION
-SELECT patent_id, cpc_group AS 'cpc_id'
+UNION ALL
+SELECT patent_id, cpc_group AS cpc_id
 FROM patview_core.g_cpc_current;
 
 -- USPC Node
 SELECT 'uspc_id','level','label'
-UNION
-SELECT DISTINCT(ifnull(uspc_mainclass_id,'')) AS 'uspc_id',
-        'mainclass' AS 'level',
-        ifnull(uspc_mainclass_title,'') AS 'label'
+UNION ALL
+SELECT DISTINCT(COALESCE(uspc_mainclass_id,'')) AS uspc_id,
+        'mainclass' AS level,
+        COALESCE(uspc_mainclass_title,'') AS label
 FROM patview_core.g_uspc_at_issue;
 
 -- USPC Edge
 SELECT 'patent_id', 'uspc_id'
-UNION
-SELECT ifnull(patent_id,''),
-        ifnull(uspc_mainclass_id,'') AS 'uspc_id'
+UNION ALL
+SELECT COALESCE(patent_id,''),
+        COALESCE(uspc_mainclass_id,'') AS uspc_id
 FROM patview_core.g_uspc_at_issue
 UNION ALL
-SELECT ifnull(patent_id,''),
-        ifnull(uspc_subclass_id,'') AS 'uspc_id'
+SELECT COALESCE(patent_id,''),
+        COALESCE(uspc_subclass_id,'') AS uspc_id
 FROM patview_core.g_uspc_at_issue;
 
 -- WIPO Node
 SELECT 'wipo_field_id', 'level', 'label'
-UNION
-SELECT DISTINCT(wipo_field_id), 
-        'field_title' AS 'level',
-        wipo_field_title AS 'label'
+UNION ALL
+SELECT DISTINCT(wipo_field_id)::VARCHAR(64), 
+        'field_title' AS level,
+        wipo_field_title AS label
 FROM patview_core.g_wipo_technology
 UNION ALL
-SELECT DISTINCT(wipo_field_id),
-        'sector_title' AS 'level',
-        wipo_sector_title AS 'label'
+SELECT DISTINCT(wipo_field_id)::VARCHAR(64),
+        'sector_title' AS level,
+        wipo_sector_title AS label
 FROM patview_core.g_wipo_technology;
 
 -- WIPO Edge
 SELECT 'patent_id', 'wipo_field_id'
-UNION
-SELECT patent_id, wipo_field_id
+UNION ALL
+SELECT patent_id, wipo_field_id::VARCHAR(64)
 FROM patview_core.g_wipo_technology;
 
 -- NBER tables removed
 
 -- IPCR Node
 SELECT 'ipcr_id', 'level', 'label'
-UNION
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'section' AS 'level', section AS 'label'
+UNION ALL
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'section' AS level, section AS label
 FROM patview_core.g_ipc_at_issue
 UNION ALL
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'ipc_class' AS 'level', ipc_class AS 'label'
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'ipc_class' AS level, ipc_class AS label
 FROM patview_core.g_ipc_at_issue
 UNION ALL
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'subclass' AS 'level', subclass AS 'label'
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'subclass' AS level, subclass AS label
 FROM patview_core.g_ipc_at_issue
 UNION ALL
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'main_group' AS 'level', main_group AS 'label'
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'main_group' AS level, main_group AS label
 FROM patview_core.g_ipc_at_issue
 UNION ALL
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'subgroup' AS 'level', subgroup AS 'label'
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'subgroup' AS level, subgroup AS label
 FROM patview_core.g_ipc_at_issue
 UNION ALL
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'symbol_position' AS 'level', symbol_position AS 'label'
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'symbol_position' AS level, symbol_position AS label
 FROM patview_core.g_ipc_at_issue
 UNION ALL
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'classification_value' AS 'level', classification_value AS 'label'
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'classification_value' AS level, classification_value AS label
 FROM patview_core.g_ipc_at_issue
 UNION ALL
-SELECT DISTINCT(ipc_sequence) AS 'ipcr_id', 'classification_data_source' AS 'level', classification_data_source AS 'label'
+SELECT DISTINCT(ipc_sequence)::VARCHAR(64) AS ipcr_id, 'classification_data_source' AS level, classification_data_source AS label
 FROM patview_core.g_ipc_at_issue;
 
 -- IPCR Edge
 SELECT 'patent_id','sequence'
-UNION
-SELECT patent_id, ipc_sequence
+UNION ALL
+SELECT patent_id, ipc_sequence::VARCHAR(64)
 FROM patview_core.g_ipc_at_issue;
