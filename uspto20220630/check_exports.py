6c06247d780d27f0f2add80c331fd8a9b5fabff0
Shaw <shaw10@iu.edu>
2023-02-09 11:47:30 -0600

    correct check_exports

--- uspto20220630/check_exports.py
+++ uspto20220630/check_exports.py
@@ -2,14 +2,14 @@ from os import listdir
 from os import popen
 import pandas as pd
 
-path = '/N/project/iuni_cadre/uspto20220630'
+path = '/N/project/iuni_cadre/uspto20220630/'
 
 files = listdir(path)
 
 for f in files:
-    df = pd.read_csv(f, sep='\t', low_memory=False)
+    df = pd.read_csv(path+f, sep='\t', low_memory=False)
     df_rows = df.shape[0]
-    f_rows = int(popen(f'wc -l {path}/{f}').read().split(' ')[0]) - 1
+    f_rows = int(popen(f'wc -l {path}{f}').read().split(' ')[0]) - 1
     if f_rows != df_rows:
         print('#'*20, f, 'number of rows do not match','#'*20)
     else:
